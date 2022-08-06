a5f9961d47b596ea83c71c7a358b5734e80a4ff7
Byun-Sung-Ho <bsh4237@gmail.com>
2022-08-07 01:28:05 +0900

    Sungho 3rd update merge

---
+++ Mazzaing/app/src/main/java/com/example/mazzaing/Createpost.kt
@@ -0,0 +1,11 @@
+package com.example.mazzaing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Createpost : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_createpost)
+    }
+}
