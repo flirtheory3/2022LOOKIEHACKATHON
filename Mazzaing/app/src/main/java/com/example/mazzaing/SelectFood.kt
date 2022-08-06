2698d483f8851d1d4a03ef2a554411ed602940b4
Byun-Sung-Ho <bsh4237@gmail.com>
2022-08-07 03:00:46 +0900

    sungho 4th update merge with WritingPage

--- Mazzaing/app/src/main/java/com/example/mazzaing/SelectFood.kt
+++ Mazzaing/app/src/main/java/com/example/mazzaing/SelectFood.kt
@@ -1,5 +1,6 @@
 package com.example.mazzaing
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_select_food.*
@@ -10,27 +11,34 @@ class SelectFood : AppCompatActivity() {
         setContentView(R.layout.activity_select_food)
 
         koreanFoodButton.setOnClickListener {
-
+            val intent = Intent(this@SelectFood, PostList::class.java)
+            startActivity(intent)
         }
 
         westernFoodButton.setOnClickListener {
+            val intent = Intent(this@SelectFood, PostListWestern::class.java)
+            startActivity(intent)
 
         }
 
         chineseFoodButton.setOnClickListener {
-
+            val intent = Intent(this@SelectFood, PostListChinese::class.java)
+            startActivity(intent)
         }
 
         asianFoodButton.setOnClickListener {
-
+            val intent = Intent(this@SelectFood, PostListAsian::class.java)
+            startActivity(intent)
         }
 
         snack_barButton.setOnClickListener {
-
+            val intent = Intent(this@SelectFood, PostListSnackbar::class.java)
+            startActivity(intent)
         }
 
         dessertButton.setOnClickListener {
-
+            val intent = Intent(this@SelectFood, PostListDessert::class.java)
+            startActivity(intent)
         }
     }
 }
